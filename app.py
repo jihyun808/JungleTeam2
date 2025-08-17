7863597eeae4e51c43e37c0eaa09fe5ef4a558f6
jihyun808 <jihyun8k@gmail.com>
2025-08-18 04:10:36 +0900

    app.py

--- app.py
+++ app.py
@@ -1,15 +1,52 @@
+# api.py
+import os
+import requests
+from flask import Flask, render_template, redirect, request, session, Blueprint, make_response
+import json
 
-from flask import Flask, send_from_directory
+os.chdir(os.path.dirname(__file__))
+with open("appdata.json", 'r', encoding='utf-8') as f:
+    appdata = json.load(f);
 
-app = Flask(__name__, static_folder='.', static_url_path='')
+from pymongo.mongo_client import MongoClient
+from routes import kakao
+from routes import jungle
+
+mongo = MongoClient(appdata["mongo"]["uri"], appdata["mongo"]["port"])
+    
+#Flask 애플리케이션 생성
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+app.config['jungle'] = appdata['jungle']
+app.config['kakao'] = appdata['kakao']
+app.config['mongo'] = mongo.jungle
+app.register_blueprint(kakao.blueprint)
+app.register_blueprint(jungle.blueprint)
+# Create a new client and connect to the server
 
 @app.route('/')
 def home():
-    return send_from_directory('.', 'index.html')
+   return render_template('index.html');
 
-@app.route('/<path:path>')
-def static_proxy(path):
-    return send_from_directory('.', path)
+@app.route('/booking')
+def booking():
+   return render_template('booking.html');
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/signin')
+def signin():
+    return render_template('signin.html')
+
+
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0',debug=True, port=35565)
+    print("server started")
+    
+    
+    
+
