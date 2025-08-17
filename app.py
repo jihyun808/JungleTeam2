0f2c86f0e53f56a8e3f82a3750bfdcbbef321f4a
jihyun808 <jihyun8k@gmail.com>
2025-08-18 04:11:01 +0900

    app.py

--- app.py
+++ app.py
@@ -26,11 +26,11 @@ app.register_blueprint(jungle.blueprint)
 
 @app.route('/')
 def home():
-   return render_template('index.html');
+   return render_template('index.html')
 
 @app.route('/booking')
 def booking():
-   return render_template('booking.html');
+   return render_template('booking.html')
 
 @app.route('/signup')
 def signup():
