802b0c541097f94da0197a52345bd97e6bacb05e
jihyun808 <jihyun8k@gmail.com>
2025-08-19 21:20:52 +0900

    Update app.py

--- SeungHoonSpace/app.py
+++ SeungHoonSpace/app.py
@@ -46,21 +46,21 @@ def mypage():
 def ticket_detail():
     param = {}
     param['data'] = {
-    "event_id": 1,
-    "stadium": "광주의 아들 김도영",
-    "price": 800,
-    "area_name": "내야 1루",
-    "date": {
-        "year": 2025,
-        "month": 8,
-        "day": 25,
-        "hour": 18,
-        "min": 30
-    },
-    "seats": [1, 2, 3],
-    "team1": "403호",
-    "team2": "401호"
-    }
+        "event_id": 1,
+        "stadium": "광주의 아들 김도영",
+        "price": 800,
+        "area_name": "내야 1루",
+        "date": {
+            "year": 2025,
+            "month": 8,
+            "day": 25,
+            "hour": 18,
+            "min": 30
+        },
+        "seats": [1, 2, 3],
+        "team1": "403호",
+        "team2": "401호"
+        }
     return render_template('ticket_detail.html', param=param)
 
 if __name__ == '__main__':
